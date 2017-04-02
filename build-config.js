2fd6f01552310da37941ebf92922fe2dc67a33ac
Francois Laberge <francois@jibo.com>
2017-04-02 15:34:04 -0400

    Switched to new school github page and added shrinkray app building support

--- build-config.js
+++ build-config.js
@@ -1,7 +1,7 @@
 ({
     baseUrl: 'src/',
     mainConfigFile: 'src/regulex.js',
-    out: 'dest/regulex.js',
+    out: 'docs/regulex.js',
     optimize: 'uglify2',
     wrap: false,
     include: ['libs/almond','regulex']
